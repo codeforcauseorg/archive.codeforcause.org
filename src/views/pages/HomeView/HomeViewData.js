aa13a9258d99a33e6252b29fee2a3a28aae4b147
Abhishek Kumar <abhimait1909@gmail.com>
2020-07-14 16:10:35 +0530

    [Refractor 1] move data to seperate files for better code visibility (#37)

---
+++ src/views/pages/HomeView/HomeViewData.js
@@ -0,0 +1,127 @@
+// mentors data----------------
+
+export const mentors = [
+  {
+    id: '1',
+    name: 'Anuj Garg',
+    avatar: '/static/images/members/anuj.png',
+    linkedin: 'https://www.linkedin.com/in/keenwarrior/'
+  },
+  {
+    id: '2',
+    name: 'Gaurav Beriwal',
+    avatar: '/static/images/members/gaurav.png',
+    linkedin: 'https://www.linkedin.com/in/gauravberiwal/'
+  },
+  {
+    id: '3',
+    name: 'Kunal Kushwaha',
+    avatar: '/static/images/members/kunal.png',
+    linkedin: 'https://www.linkedin.com/in/kunal-kushwaha/'
+  },
+  {
+    id: '4',
+    name: 'Ganga Chaturvedi',
+    avatar: '/static/images/members/ganga.png',
+    linkedin: 'https://www.linkedin.com/in/gangachatrvedi/'
+  },
+  {
+    id: '5',
+    name: 'Ekta Mishra',
+    avatar: '/static/images/members/ekta.png',
+    linkedin: 'https://www.linkedin.com/in/darecoder/'
+  },
+  {
+    id: '6',
+    name: 'Bharat Bhardwaj',
+    avatar: '/static/images/members/bharat.png',
+    linkedin: 'https://www.linkedin.com/in/bharatbbhardwaj/'
+  }
+];
+
+// experience data ---------------------
+
+export const experience = [
+  {
+    id: '1',
+    img: '/static/images/experience/students.svg',
+    count: '5000+',
+    title: 'Students'
+  },
+  {
+    id: '2',
+    img: '/static/images/experience/teaching_hours.svg',
+    count: '3200+',
+    title: ' Teaching Hours'
+  },
+  {
+    id: '3',
+    img: '/static/images/experience/projects.svg',
+    count: '1000+',
+    title: 'Projects'
+  },
+  {
+    id: '4',
+    img: '/static/images/experience/contributors.svg',
+    count: '100+',
+    title: 'Contributers'
+  }
+];
+
+// Open source section data-----------------
+export const openKnowledgeContent = {
+  title: 'Open Knowledge',
+  about:
+    'Our structure is based on generating the knowledge flow that is not limited by bottlenecks. So we expand our mentor base with every student we teach. We inspire our mentees to pass on the guidance and support.',
+  points: [
+    {
+      id: '1',
+      img: '/static/images/icons/ok1.svg',
+      content:
+        'We host live classes and workshops regularly with focus on hands-on learning, which is crucial in gaining real-world software development experience.'
+    },
+    {
+      id: '2',
+      img: '/static/images/icons/ok2.svg',
+      content:
+        'Get connected to a large group of developers and open-source enthusiasts to get all your doubts resolved - whether technical, career-oriented or soft skills.'
+    },
+    {
+      id: '3',
+      img: '/static/images/icons/ok3.svg',
+      content:
+        'Learn from the experiences of the community members in the industry which will help you with career guidance for the path best suited to you.'
+    }
+  ]
+};
+
+export const openSourceContent = {
+  title: 'Open Source',
+  about:
+    'Our motivation is to funnel learning through OSS development that can solve real world problems. With this you can enhance your ideation, development and soft skills like communication, teamwork and leadership.',
+  points: [
+    {
+      id: '1',
+      img: '/static/images/icons/os1.svg',
+      content:
+        'Practical development experience is crucial in becoming a pragmatic programmer. Join us and develop products to enhance your skill set for real.'
+    },
+    {
+      id: '2',
+      img: '/static/images/icons/os2.svg',
+      content:
+        'When talking about graphs, the edges matter as much as the nodes. We follow Collaborative Working that helps you generate showcasable work.'
+    },
+    {
+      id: '3',
+      img: '/static/images/icons/os3.svg',
+      content:
+        'Software development professionals flourish by networking. Get connected to people who matter by our online/offline events.'
+    }
+  ]
+};
+
+export const whatWeStandForDescription = ' We aim to provide a real world practical learning experience and'
+  + 'keep students informed about the latest trends in technology,'
+  + 'open-source and opportunities, so that they can keep up with the'
+  + 'fast-paced digital world by following a pi-shaped learning pattern.';
