1c07fd480afa4177edfeaf4a39685ee77341951e
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-07-25 01:31:10 +0530

    add responsive standforpage, to show fully aligned content on medium and smaller screens

--- src/components/AboutCard/SubCard.js
+++ src/components/AboutCard/SubCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Grid, Typography, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     padding: 0,
-    paddingTop: '40px'
+    paddingTop: '40px',
   },
 
   extraMargin: {
     marginLeft: 15,
+    float: 'right',
     [theme.breakpoints.down('md')]: {
       marginLeft: 25
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0,
+      fontSize: '85%'
     }
+  },
+  image: {
+    margin: '10px auto',
+    display: 'inline-block',
+    float: 'left',
   }
 }));
 
@@ -19,10 +29,10 @@ const SubCard = ({ point }) => {
   const classes = useStyles();
   return (
     <Grid container className={classes.root}>
-      <Grid item md={2}>
+      <Grid className={classes.image} item sm={2}>
         <img alt="Select file" height="55px" src={point.img} />
       </Grid>
-      <Grid item md={10}>
+      <Grid item sm={10}>
         <Typography
           className={classes.extraMargin}
           align="justify"
