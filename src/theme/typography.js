3d849592082f76887590a033c49e9dfc82a8f468
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-08-09 22:16:14 +0530

    Add Montserrat font family

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -2,34 +2,41 @@ export default {
   h1: {
     fontWeight: 500,
     fontSize: 35,
-    letterSpacing: '-0.24px'
+    letterSpacing: '-0.24px',
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
   },
   h2: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500,
     fontSize: 29,
     letterSpacing: '-0.24px'
   },
   h3: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500,
     fontSize: 24,
     letterSpacing: '-0.06px'
   },
   h4: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500,
     fontSize: 20,
     letterSpacing: '-0.06px'
   },
   h5: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500,
     fontSize: 16,
     letterSpacing: '-0.05px'
   },
   h6: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500,
     fontSize: 14,
     letterSpacing: '-0.05px'
   },
   overline: {
+    fontFamily: `"Montserrat", "Roboto", sans-serif`,
     fontWeight: 500
   }
 };
