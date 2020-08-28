0f3d06ff2af288cfc3c0e32b0ff3851e726c4d33
Ganga <ganga.chaturvedi24@gmail.com>
2020-08-29 00:16:39 +0530

    Adds application form for Campus leaders

---
+++ src/views/pages/CLView/ApplyNowModal.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const useStyles = makeStyles(theme => ({
+
+    btn: {
+        backgroundColor: '#A60000',
+        color: '#ffffff',
+        textTransform: 'capitalize',
+        [theme.breakpoints.down('sm')]: {
+            width: '100%'
+        },
+        '&:hover': {
+            backgroundColor: 'rgba(166, 0, 0, 0.8)'
+        }
+    },
+
+}));
+
+export default function ApplyNowModal() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button className={classes.btn}
+                size="large"
+                variant="contained"
+                onClick={handleClickOpen}>
+                Apply now
+      </Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To subscribe to this website, please enter your email address here. We will send updates
+                        occasionally.
+          </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+          </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Subscribe
+          </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
