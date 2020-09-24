672eac819d5bf3c92eb8d956ea344578802fc4b2
KeenWarrior <anujgargcse@gmail.com>
2020-09-24 13:10:29 +0530

    Updates font weights to create suitable view

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -2,22 +2,22 @@ export default {
   fontFamily: ['"Montserrat"'].join(','),
 
   h1: {
-    fontWeight: 500,
+    fontWeight: 600,
     fontSize: 35,
     letterSpacing: '-0.24px'
   },
   h2: {
-    fontWeight: 500,
+    fontWeight: 600,
     fontSize: 29,
     letterSpacing: '-0.24px'
   },
   h3: {
-    fontWeight: 500,
+    fontWeight: 600,
     fontSize: 24,
     letterSpacing: '-0.06px'
   },
   h4: {
-    fontWeight: 500,
+    fontWeight: 600,
     fontSize: 20,
     letterSpacing: '-0.06px'
   },
@@ -28,6 +28,16 @@ export default {
   },
   h6: {
     fontWeight: 600,
+    fontSize: 14,
+    letterSpacing: '-0.05px'
+  },
+  body1: {
+    fontWeight: 400,
+    fontSize: 16,
+    letterSpacing: '-0.05px'
+  },
+  body2: {
+    fontWeight: 400,
     fontSize: 14,
     letterSpacing: '-0.05px'
   },
