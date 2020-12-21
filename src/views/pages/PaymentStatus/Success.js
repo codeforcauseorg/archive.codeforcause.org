f74bf65fa8d76b4a2ec8e06502ad6dcda001c3a1
Anuj Garg <anujgargcse@gmail.com>
2020-12-21 17:25:21 +0530

    Update Success.js

--- src/views/pages/PaymentStatus/Success.js
+++ src/views/pages/PaymentStatus/Success.js
@@ -17,12 +17,14 @@ export default function Success() {
     function () {
       removeWait(false);
     }, 
-  5000)
+  7000)
 
   if (wait) {
     return (
       <div className={classes.root}>
-        <Typography align="center">Your last payment to codeforcause was successful. Redirecting to home page...</Typography>
+        <Typography align="center">Your last payment to codeforcause was successful. We will connect you within 24 hours for further details.</Typography>
+        <Typography align="center">Connect with us is case of any doubt.</Typography>
+        <Typography align="center">Redirecting to home page...</Typography>
       </div>
     );
   }
