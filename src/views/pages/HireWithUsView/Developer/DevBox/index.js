16acc5729c9810f38a79a05152fdb53f35ce4158
atharmohammad <mohd.rule123@gmail.com>
2021-02-06 22:16:32 +0530

    Add Component in HireWithUs

---
+++ src/views/pages/HireWithUsView/Developer/DevBox/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import classes from './DevBox.module.css'
+import Gold_Ribbon from '../../../../../assets/images/Gold_Ribbon.png'
+import Book_Icon from '../../../../../assets/images/Book_Icon.png'
+import Thumbs_Up from '../../../../../assets/images/Thumbs_Up.png'
+
+
+const devBox = (props)=>{
+  let img;
+  switch(props.Img){
+    case('Gold_Ribbon') :{
+      img = Gold_Ribbon;
+      break;
+    }
+    case('Book_Icon') : {
+      img = Book_Icon;
+      break;
+    }
+    case('Thumbs_Up'):{
+      img=Thumbs_Up;
+      break;
+    }
+    default:img=Thumbs_Up
+  }
+
+  return (
+    <div>
+      <div className={classes.DevBox}>
+        <div className={classes.Content}>
+          <div>
+            <img src={img} className={classes.Gold_Ribbon} alt="Loading Image!"/>
+          </div>
+          <strong><p>1000+</p></strong>
+          <strong><p>Problems Solved</p></strong>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default devBox;
---
+++ src/views/pages/HireWithUsView/Developer/Developer.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import Devbox from './DevBox/index';
+import classes from './Developer.module.css';
+
+class Developer extends Component{
+  render(){
+
+
+    return (
+
+      <div>
+        <div className={classes.Developer}>
+
+        <div>
+          <p><span className={classes.heading1}>The Making Of </span>
+            <span className={classes.heading2}>The Fine Developer</span></p>
+        </div>
+            <div className={classes.Box}>
+              <Devbox Img='Gold_Ribbon'/>
+              <Devbox Img='Book_Icon'/>
+              <Devbox Img='Thumbs_Up'/>
+          </div>
+
+          <div className={classes.Bottom}>
+            <Devbox/>
+            <div className={classes.BottomBox}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset ke Aldus PageMaker including versions of Lorem Ipsum elease of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.
+            </div>
+
+          </div>
+
+        </div>
+          <div className={classes.SideBox} />
+      </div>
+    )
+  }
+}
+
+export default Developer;
