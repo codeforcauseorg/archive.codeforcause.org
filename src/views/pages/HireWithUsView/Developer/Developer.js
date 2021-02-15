02706f13de26a7251136ab80c574ddfa234d6fbd
atharmohammad <mohd.rule123@gmail.com>
2021-02-15 14:08:12 +0530

    Improvements

--- src/views/pages/HireWithUsView/Developer/Developer.js
+++ src/views/pages/HireWithUsView/Developer/Developer.js
@@ -165,7 +165,13 @@ export default function Developer(props) {
         <Grid item xs={12} sm={12} className={classes.Bottom}>
           <Grid item  className={classes.BottomBox}>
             <Typography variant="body1" class={classes.Write}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently ons of Lorem Ipsum.
+            Lorem Ipsum is simply dummy. Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen
+              book. It has survived not only five centuries, but also the leap
+              into electronic typesetting, remaining essentially unchanged. It
+              was popularised in the 1960s with the release of Letraset sheets
+              cont{' '}
             </Typography>
           </Grid>
           <div className={classes.SideBox} />
