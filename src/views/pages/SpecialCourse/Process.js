2d80595a39fc9a1b8fc2fb56be19c8160eaa3d34
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-04-11 16:42:15 +0530

    Temporarily disabling ending today on c++

--- src/views/pages/SpecialCourse/Process.js
+++ src/views/pages/SpecialCourse/Process.js
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 import ProcessSteppers from './ProcessSteppers';
-import Countdown from 'react-countdown';
+// import Countdown from 'react-countdown'; // temporarily disabled
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -46,43 +46,47 @@ function Process({ course, className, ...rest }) {
             </Grid>
           </React.Fragment>
         ) : (
-          <Countdown
-            date={Date.parse(course.discountEnds)}
-            renderer={renderer}
-          />
+          ''
+          // Temporarily disabled...............
+          // <Countdown 
+          //   date={Date.parse(course.discountEnds)}
+          //   renderer={renderer}
+          // />
         )}
       </Container>
     </div>
   );
 }
 
-const Completionist = () => (
-  <Typography
-    style={{ marginTop: '32px', color: '#ffffff' }}
-    variant="h1"
-    align="center"
-    gutterBottom
-  >
-    Offer Expired
-  </Typography>
-);
+// const Completionist = () => (
+//   <Typography
+//     style={{ marginTop: '32px', color: '#ffffff' }}
+//     variant="h1"
+//     align="center"
+//     gutterBottom
+//   >
+//     Offer Expired
+//   </Typography>
+// );
 
-const renderer = ({ days, completed }) => {
-  if (completed) {
-    return <Completionist />;
-  } else {
-    return (
-      <Typography
-        style={{ marginTop: '32px', color: '#ffffff' }}
-        variant="h1"
-        align="center"
-        gutterBottom
-      >
-        Ending {days === 0 ? 'Today' : `in ${days} days`}
-      </Typography>
-    );
-  }
-};
+// Temporarily disabled.......................................................................................................
+
+// const renderer = ({ days, completed }) => {
+//   if (completed) {
+//     return <Completionist />;
+//   } else {
+//     return (
+//       <Typography
+//         style={{ marginTop: '32px', color: '#ffffff' }}
+//         variant="h1"
+//         align="center"
+//         gutterBottom
+//       >
+//         Ending {days === 0 ? 'Today' : `in ${days} days`}
+//       </Typography>
+//     );
+//   }
+// };
 
 Process.propTypes = {
   className: PropTypes.string
