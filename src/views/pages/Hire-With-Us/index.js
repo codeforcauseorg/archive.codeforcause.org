7e5739d538c1de6af4de878b05fd84ff5f338ca1
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-04-15 21:42:38 +0530

    Add other sections

--- src/views/pages/Hire-With-Us/index.js
+++ src/views/pages/Hire-With-Us/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import TopMain from "./TopMain";
+import Developer from '../HireWithUsView/Developer/Developer';
+import Major from '../HireWithUsView/Major/Major';
+import Philosphy from '../HireWithUsView/Philosphy/Philosphy';
+import Footer from '../common/Footer';
+
 function HIRE_WITH_US() {
     return (
         <div>
             <TopMain />
+
+            <Developer />
+            <Philosphy />
+            <Major />
+            <Footer />
         </div>
     )
 }
