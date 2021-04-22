cf544df935d42c5fc14511e47e38cad584a11737
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-04-22 15:33:54 +0530

    Remove Horizontal Scrolling

--- src/views/pages/Hire-With-Us/index.js
+++ src/views/pages/Hire-With-Us/index.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import TopMain from "./TopMain";
+import React from 'react';
+import TopMain from './TopMain';
 import Developer from '../HireWithUsView/Developer/Developer';
-import HireAtEase from "./HireAtEase";
+import HireAtEase from './HireAtEase';
 import Major from '../HireWithUsView/Major/Major';
 import Philosphy from '../HireWithUsView/Philosphy/Philosphy';
 import Footer from '../common/Footer';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    overflow: 'hidden'
+  }
+}));
 
 function HIRE_WITH_US() {
-    return (
-        <div>
-            <TopMain />
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <TopMain />
 
-            <Developer />
-            <HireAtEase />
-            <Philosphy />
-            <Major />
-            <Footer />
-        </div>
-    )
+      <Developer />
+      <HireAtEase />
+      <Philosphy />
+      <Major />
+      <Footer />
+    </div>
+  );
 }
 
-export default HIRE_WITH_US
+export default HIRE_WITH_US;
