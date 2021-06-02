d7f76d7de2d757ac309c025317f32252b7d1459d
vasudev19126 <vasudev19126@iiitd.ac.in>
2021-06-02 12:13:09 +0530

    replace course page banner with Hero

--- src/views/pages/CoursesView/index.js
+++ src/views/pages/CoursesView/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
-// import Hero from './Hero';
+import Hero from './Hero';
 import Footer from '../common/Footer';
 import CTA from './CTA';
 import Courses from './Courses';
 import Apply from '../common/Apply';
-import Banner from '../../../components/Banner';
+// import Banner from '../../../components/Banner';
 import Recommendations from '../common/Recommendations';
 
 const useStyles = makeStyles(() => ({
@@ -18,8 +18,8 @@ function CoursesView() {
 
   return (
     <Page className={classes.root} title="Courses">
-      <Banner/>
-      {/* <Hero /> */}
+      {/* <Banner/> */}
+      <Hero />
       <Courses />
       <Recommendations />
       <Apply />
